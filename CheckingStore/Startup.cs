956c3f11fcc29fac8ef0647f2ce89ba30b6fb5b7
NebeskyiYevhenii <evheniy.slav@gmail.com>
2021-01-19 15:29:18 +0200

    first commit

---
+++ CheckingStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CheckingStore.Startup))]
+namespace CheckingStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
